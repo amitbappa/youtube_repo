b95f1001d2bfd005df1814b80b59cb5155c461a1
amit <amitbappa@gmail.com>
2020-08-24 11:30:44 +0300

    commit  name changes

--- app/src/main/java/com/demo/videoplay/Constants.java
+++ app/src/main/java/com/demo/videoplay/Constants.java
@@ -1,15 +1,12 @@
 package com.demo.videoplay;
 
 public class Constants {
-    public static final String SERVER_URL = "https://a3eed115f800.ngrok.io";
+    public static final String SERVER_URL = "https://3b3d9c575436.ngrok.io";
 
     public static  final String EVENT_STREAM_STARTED = "STREAM_STARTED";
     public static  final String EVENT_SOCKET_DISCONNECTED= "SOCKET_DISCONNECTED";
     public static  final String EVENT_ROOM_CREATED = "room_created";
     public static  final String EVENT_ROOM_JOINED = "room_joined";
-    public static  final String EVENT_ROOM_START_CALL = "start_call";
-    public static  final String EVENT_VIDEO_OFFER = "video_offer";
-    public static  final String EVENT_VIDEO_PLAY = "video_play";
     public static  final String EVENT_ROOM_FULL = "room_full";
     public static  final String EVENT_SET_CURRENT_VIDEO = "set_current";
     public static  final String EVENT_GET_CURRENT_VIDEO = "get_current";
